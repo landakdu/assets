047b4185542a0bdea273cb94f52c191343302ac0
landakdu <durilandak429@gmail.com>
2025-11-14 09:02:24 +0800

    Update referral.js

--- referral.js
+++ referral.js
@@ -4,7 +4,7 @@ function linktokekwin() {location.replace("http://panenmahkota.lol/daftar?ref=ba
 function linkpphoki() {location.replace("https://pphoki8899.xyz/?ref=babisexy");}
 function linkaw8() {location.replace("https://www.aw8jos.com/id-id/register?refid=ymbrprab");}
 function linkhoki177() {location.replace("https://halohoki177.com/register?ref=babisexy");}
-function linknewliga365() {location.replace("https://newliga365y.one/?action=register&rc=LEOK60YFBD&ci=11");}
+function linknewliga365() {location.replace("https://newliga365q.org/?action=register&rc=LEOK60YFBD&ci=11");}
 function linklotto01() {location.replace("https://masterlotto01.org/referral/BABISEXY");}
 function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/daftar?ref=babisexy");}
 function linkaquaslot() {location.replace("https://aquaslot8.xyz/?ref=babisexy");}
@@ -13,12 +13,13 @@ function linkkadobet() {location.replace("https://rodakadobet.com/?ref=babisexy"
 function linkmpo222() {location.replace("https://mpo222promax.com/register/JK86G0Y4");}
 function linkqqbos99() {location.replace("https://www.senisastra.com/Member/StreamlinedRegister?R=0B6343ED");}
 function link303hoki() {location.replace("https://eworldexternal.com/register/B48RPJDN");}
-function linkjet88bet() {location.replace("https://jet88betadapsi.com/?ref=babisexy");}
+function linkjet88bet() {location.replace("https://jet88enak.space/?ref=babisexy");}
 function linkmpo369() {location.replace("https://mpo369bj.com/register/JQPK80KM");}
 function link8bettingan() {location.replace("https://link1.bettingan.vip/");}
 function linkmpo08() {location.replace("https://mpo08bul.xyz/register/Q2RDGN20");}
 function linkmpo888() {location.replace("https://lp1.mpo888games.link/register/437GR640");}
 function linkdewatogel() {location.replace("https://dwtmaxwinjp.space/register?referral_code=babisexy");}
+
 
 
 
