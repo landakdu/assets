ee0cb51d953f1e83efa48ec2bb4e7cd5b2b79a09
landakdu <durilandak429@gmail.com>
2025-12-02 21:35:58 +0800

    Update referral.js

--- referral.js
+++ referral.js
@@ -5,7 +5,7 @@ function linktokekwin() {location.replace("http://gidx-tokek1.xyz/daftar?ref=bab
 function linkpphoki() {location.replace("https://pphoki8899.xyz/?ref=babisexy");}
 function linkaw8() {location.replace("https://www.aw8petir.com/id-id/register?refid=ymbrprab");}
 function linkhoki177() {location.replace("https://halohoki177.com/register?ref=babisexy");}
-function linknewliga365() {location.replace("https://newliga365vip.site/?action=register&rc=LEOK60YFBD&ci=11");}
+function linknewliga365() {location.replace("https://newliga365v.ink/?action=register&rc=LEOK60YFBD&ci=11");}
 function linklotto01() {location.replace("https://masterlotto01.org/referral/BABISEXY");}
 function linkkijangwin() {location.replace("http://spiritkijangwin-1reff.shop/daftar?ref=babisexy");}
 function linkaquaslot() {location.replace("https://aquaslot9.xyz/?ref=babisexy");}
@@ -15,7 +15,7 @@ function linkmpo222() {location.replace("https://mpo222emas.com/register/JK86G0Y
 function linkqqbos99() {location.replace("https://www.senisastra.com/");}
 function linkqqbos99x() {location.replace("https://www.senisastra.com/");}
 function link303hoki() {location.replace("https://eworldexternal.com/register/B48RPJDN");}
-function linkjet88bet() {location.replace("https://jet88enak.space/?ref=babisexy");}
+function linkjet88bet() {location.replace("https://jet88curl.site/?ref=babisexy");}
 function linkmpo369() {location.replace("https://mpo369bj.com/register/JQPK80KM");}
 function link8bettingan() {location.replace("https://link1.bettingan.vip/");}
 function linkmpo08() {location.replace("https://mpo08dna.xyz/register/Q2RDGN20");}
@@ -200,6 +200,7 @@ function linkkw303() {location.replace("https://anianiwangi.pages.dev/");}
 function linkcumi787() {location.replace("https://www.cumi787.org/amp/");}
 function linkterminal4d() {location.replace("https://linktr.ee/terminal4d_1");}
 function linkrajajudi33() {location.replace("https://ampnewrajajudi33.com/");}
+
 
 
 
