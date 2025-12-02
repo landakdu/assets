c176cb1712f433b7a6a957a1a6e5d39a17d1807b
landakdu <durilandak429@gmail.com>
2025-12-03 00:21:09 +0800

    Update referral.js

--- referral.js
+++ referral.js
@@ -100,7 +100,7 @@ function linkbetcoinasia() {location.replace("https://saldodana.com/betcoin/");}
 function linkkaikoslot() {location.replace("https://kaikoslot-login.pages.dev/amp");}
 function linkmega55() {location.replace("https://atlantaotology.com/meet-amp/");}
 function linkwishslot() {location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=13470840&group=0");}
-function linkpintutogel() {location.replace("https://afilibahis.com/");}
+function linkpintutogel() {location.replace("https://secure.livechatinc.com/customer/action/open_chat?license_id=14193819&group=0");}
 function linknyaitogel() {location.replace("https://sigsauergunsale.com/");}
 function linkaurajp() {location.replace("https://aurajpx.theamplink.com/");}
 function linkdauntogel() {location.replace("https://heylink.me/dauntogel.link.alternatif/");}
@@ -200,6 +200,7 @@ function linkkw303() {location.replace("https://anianiwangi.pages.dev/");}
 function linkcumi787() {location.replace("https://www.cumi787.org/amp/");}
 function linkterminal4d() {location.replace("https://linktr.ee/terminal4d_1");}
 function linkrajajudi33() {location.replace("https://ampnewrajajudi33.com/");}
+
 
 
 
