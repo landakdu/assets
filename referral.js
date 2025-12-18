777791ead34a41ec17b7866708f4e4a8e2e02b71
landakdu <durilandak429@gmail.com>
2025-12-19 00:30:39 +0800

    Update referral.js

--- referral.js
+++ referral.js
@@ -12,7 +12,7 @@ function linkcoblos4d() {location.replace("https://v15.coblos4dbet.site/?ref=bab
 function linkkadobet() {location.replace("https://rodakadobet.com/?ref=babisexy");}
 function linkmpo222() {location.replace("https://heylink.me/mpo222gacor/");}
 function link303hoki() {location.replace("https://eworldexternal.com/register/B48RPJDN");}
-function linkjet88bet() {location.replace("https://jet88curl.site/?ref=babisexy");}
+function linkjet88bet() {location.replace("https://linklist.bio/jet88bet");}
 function linkmpo369() {location.replace("https://mpo369bj.com/register/JQPK80KM");}
 function link8bettingan() {location.replace("https://link1.bettingan.vip/");}
 function linkmpo08() {location.replace("https://mpo08dna.xyz/register/Q2RDGN20");}
@@ -198,6 +198,7 @@ function linkkw303() {location.replace("https://anianiwangi.pages.dev/");}
 function linkcumi787() {location.replace("https://www.cumi787.org/amp/");}
 function linkterminal4d() {location.replace("https://linktr.ee/terminal4d_1");}
 function linkrajajudi33() {location.replace("https://ampnewrajajudi33.com/");}
+
 
 
 
