cca4dffb7bb2c4e02b16cb2e9bdc2995f3eeb748
landakdu <durilandak429@gmail.com>
2025-12-25 17:07:14 +0800

    Update referral.js

--- referral.js
+++ referral.js
@@ -7,6 +7,8 @@ Saya sudah bersusah payah membuat landing page dengan tujuan mencari sedikit rez
 */
 
 function sewanamax() {window.open("https://tawk.to/chat/65eec9ce8d261e1b5f6b705b/1homc8jr0?");}
+function link() {window.open("#");}
+function linkleoslot88() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=14859759&group=0");}
 function linkwdyuk() {window.open("https://wdyukaja.com/20241114/?referralid=babisexy");}
 function linkvespa69() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=15586902&group=0");}
 function linknuhunslot() {window.open("https://nuhunslot.co/?Ref=8AuFNHky");}
@@ -224,4 +226,5 @@ function linkkw303() {window.open("https://situskw303.com/?ref=babisexy");}
 function linkcumi787() {window.open("https://1cumi787.forum/daftar?ref_id=d58f8fa7");}
 function linkterminal4d() {window.open("https://terminal4dtoto.online/register?referral_code=babisexy");}
 function linkrajajudi33() {window.open("https://api.whatsapp.com/send/?phone=6287856319466");}
+
 
