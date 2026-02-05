47305dd0f7975fd7acdbda95180c4bf42592873d
bunsin0309 <bunsin0309@gmail.com>
2026-02-05 21:00:18 +0800

    Update referral.js

--- referral.js
+++ referral.js
@@ -64,7 +64,7 @@ function linkdewatogel() {window.open("https://besdp46516.dwtku.info/register?re
 function linkwis77() {window.open("https://wis77-mighty.com/register/MJ54RJPR");}
 function linkbento4d() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=12186300&group=0");}
 function linkmpo111() {window.open("https://mpo111satu.xyz/register/67053GLN");}
-function linkzona66() {window.open("https://zona66.link/register/L7NQ2837");}
+function linkzona66() {window.open("https://app.chaport.com/widget/show.html?appid=662499fd96a23d6d64360a18&");}
 function linkgacorbos88() {window.open("https://sprg.gacorbos88pl.com/register?ref=GS2ABAD018U7");}
 function linkmusangwin() {window.open("https://www.koilmusangwin.com/register?ref=2M3TNAFG0EVH");}
 function linkdemen303() {window.open("https://demen303old.live/register?ref=Tgh1wAX");}
@@ -241,6 +241,7 @@ function linkkw303() {window.open("https://kw303.live/?ref=babisexy");}
 function linkcumi787() {window.open("https://3cumi787.store/daftar?ref_id=d58f8fa7");}
 function linkterminal4d() {window.open("https://terminal4dresmi.xyz/register?referral_code=babisexy");}
 function linkrajajudi33() {window.open("https://secure.livechatinc.com/customer/action/open_chat?license_id=19458017&group=0");}
+
 
 
 
